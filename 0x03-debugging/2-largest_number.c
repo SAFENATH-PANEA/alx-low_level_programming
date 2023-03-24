51cb39ceac8b21ad176df995c5c5d30b78f6e0ba
SAFENATH-PANEA <wekesajoseph31@gmail.com>
2023-03-24 10:33:23 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,19 +16,21 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
+	else if (a > c && c > b)
+		largest = a;
+	else if (b > a && a > c)
 	{
-		largest = c;
-	}
-	else if (b > c)
-	{	
 		largest = b;
-	}	
-	else
-	{
-		largest = c;
 	}
-
-return (largest);
+	else if (b > c && c > a)
+		largest = b;
+	else if (c > a && a > b)
+		largest = c;
+	else 
+	{	
+		largest = c;
+	}	
+	
+	return (largest);
 }
 
